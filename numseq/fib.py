71d7f31517b11ef1d381a2e022fc74a82dba0b43
Nick D <nmdanner123@gmail.com>
2019-06-12 06:48:17 -0400

    fixing docstrings

--- numseq/fib.py
+++ numseq/fib.py
@@ -1,4 +1,7 @@
 from math import sqrt
 
+
 def fib(n):
+    '''will return the nth number in the fibonacci sequence'''
+
     return ((1+sqrt(5))**n-(1-sqrt(5))**n)/(2**n*sqrt(5))
--- numseq/geo.py
+++ numseq/geo.py
@@ -1,8 +1,15 @@
 def square(n):
+    '''will square input'''
+
     return n ** 2
 
+
 def triangle(n):
+
     return sum(list(range(n + 1)))
 
+
 def cube(n):
+    '''will cube input'''
+
     return n ** 3
